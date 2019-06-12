6eea92c12e2281fdd854fb065f28ca36daa0b83e
Andrés Cifuentes <aecifuentesv@gmail.com>
2019-06-12 16:34:45 -0400

    Libreria de hace installable por pip

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
+# -*- coding: utf-8 -*-
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='mp-wrapper',  
-     version='0.1',
-     scripts=['mp-wrapper'] ,
-     author="Andrés Cifuentes",
-     author_email="aecifuentesv@gmail.com",
-     description="Wrapper para Mercado Pago para librerias API",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 2.7",
-         "Operating System :: OS Independent",
-     ],
+    name='mp-wrapper',  
+    version='0.1',
+    author="Andrés Cifuentes",
+    author_email="aecifuentesv@gmail.com",
+    description="Wrapper para Mercado Pago para librerias API",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
  )
